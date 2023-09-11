22abe25ac0ecd5ef6d837101b150bd825882665d
rupal3019 <rupal.grover3019@gmail.com>
2023-09-11 13:00:49 +0530

    Added seeders

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ const express = require('express');
 const bodyParser = require("body-parser");
 
 const {PORT} = require('./config/serverConfig');
+
+// const db = require('./models/index')
+const {City,Airport} = require('./models/index');
 const ApiRoutes = require('./routes/index');
 
 const setupAndStartServer = async() => {
@@ -13,8 +16,14 @@ const setupAndStartServer = async() => {
     app.use(bodyParser.urlencoded({extended:true}));
     app.use('/api',ApiRoutes);
 
-    app.listen(PORT, () => {
+    app.listen(PORT, async () => {
         console.log(`Server started at ${PORT}`);
+
+        if(process.env.SYNC_DB){
+            db.sequelize.sync({alter: true});
+        }
+        
+
     })
 }
 
---
+++ src/seeders/20230911063125-add-airports.js
@@ -0,0 +1,58 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add seed commands here.
+     *
+     * Example:
+     * await queryInterface.bulkInsert('People', [{
+     *   name: 'John Doe',
+     *   isBetaMember: false
+     * }], {});
+    */
+
+    await queryInterface.bulkInsert('Airports', [{
+      name: 'Begumpet Airport',
+      cityId: 10,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Indira Gandhi International Airport',
+      cityId: 1,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Ramagundam Airport',
+      cityId: 10,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Hadapsar Airport',
+      cityId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+    {
+      name: 'Osmanabad Airport',
+      cityId: 8,
+      createdAt: new Date(),
+      updatedAt: new Date()
+    },
+  ],{});
+
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add commands to revert seed here.
+     *
+     * Example:
+     * await queryInterface.bulkDelete('People', null, {});
+     */
+  }
+};
